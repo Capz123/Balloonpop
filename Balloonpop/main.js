851d50f949a34a09d33d83af55ac658b4e9b70de
Capz123 <capuzangelo23@gmail.com>
2022-06-16 11:27:44 +0800

    infationRate

--- Balloonpop/main.js
+++ Balloonpop/main.js
@@ -1,5 +1,23 @@
+let clickCount = 0
+let height = 120
+let width = 100
+let inflationRate = 20
+let maxSize = 300
+
 function Inflate() {
-    var balloonElement = document.getElementById("Balloon")
-    balloonElement.style.height = "500px"
-    balloonElement.style.width = "300px"
+    clickCount++
+    let balloonElement = document.getElementById("Balloon")
+    height += inflationRate
+    width += inflationRate
+    if (height >= maxSize) {
+        console.log("pop the baloon")
+        height = 0
+        width = 0
+    }
+    balloonElement.style.height = height + "px"
+    balloonElement.style.width = width + "px"
+
+
+    let clickCountElement = document.getElementById("click-count")
+    clickCountElement.innerText = clickCount
 }
