4b3c9597269233df3e7412ec3e3bc48a21ae8766
Capz123 <capuzangelo23@gmail.com>
2022-06-27 16:41:54 +0800

    addJS

--- Balloonpop/main.js
+++ Balloonpop/main.js
@@ -3,6 +3,15 @@ let height = 120
 let width = 100
 let inflationRate = 20
 let maxSize = 300
+let popCount = 0
+
+function startGame() {
+    // console.log("time to start")
+    setTimeout(() => {
+        console.log("it's been three seconds")
+    }, 3000)
+
+}
 
 function Inflate() {
     clickCount++
@@ -11,8 +20,10 @@ function Inflate() {
     width += inflationRate
     if (height >= maxSize) {
         console.log("pop the baloon")
+        popCount++
         height = 0
         width = 0
+        document.getElementById("pop-count").innerText = popCount
     }
     balloonElement.style.height = height + "px"
     balloonElement.style.width = width + "px"
@@ -21,3 +32,4 @@ function Inflate() {
     let clickCountElement = document.getElementById("click-count")
     clickCountElement.innerText = clickCount
 }
+
